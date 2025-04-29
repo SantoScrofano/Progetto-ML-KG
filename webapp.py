ee1f9db2d587a2188415208dc9ac2d3dd326916c
SantoScrofano <santo.scrofano@gmail.com>
2025-04-29 12:45:22 +0200

    webapp.py

--- webapp.py
+++ webapp.py
@@ -26,9 +26,9 @@ def richiesta_input():
 def spiega_risultato(predizione):
      st.write("Risultato:")
      if predizione > 0.1 :
-         st.write(" Secondo il modello ingrasserai di",{round(predizione)}, "KG! ")
+         st.write(" Secondo il modello ingrasserai di",{round(predizione,2)}, "KG! ")
      elif predizione < -0.1 :
-         st.write(" Secondo il modello dimagrirai di",{round(abs(predizione))}, "KG! ")
+         st.write(" Secondo il modello dimagrirai di",{round(abs(predizione,2))}, "KG! ")
      else:
          st.write("Avrai delle variazioni minime, pressocchè nulle. Sei costante nel tempo ! ")
 
